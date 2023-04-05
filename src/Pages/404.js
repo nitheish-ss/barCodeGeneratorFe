4f1a63f6494be422f9b5e55925862d4c0a50d3e5
nitheish-ss <nitheishm@gmail.com>
2023-04-05 19:04:57 +0530

    4o4

--- src/Pages/404.js
+++ src/Pages/404.js
@@ -5,13 +5,7 @@ const NotFound = () => (
       <div className="container flex">
         <div className="error_content_two text-center">
           <h2>Error. We can’t ftind the page you’re looking for.</h2>
-          <p>
-            Sorry for the inconvenience. Go to our homepage or check out our
-            latest collections for Fashion, Chair, Decoration...{" "}
-          </p>
-          <form action="#" className="search">
-            <input type="text" className="form-control" placeholder="search" />
-          </form>
+          <p>Sorry for the inconvenience. Go to our homepage</p>
           <a href="/" className="about_btn btn_hover">
             Back to Home Page <i className="arrow_right"></i>
           </a>
