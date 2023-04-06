bc1cabb847c4fa1ef8c65990002f50447a5449fd
nitheish-ss <nitheishm@gmail.com>
2023-04-06 15:23:00 +0530

    new scanner test

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,45 +1,23 @@
-import { returnStatement } from "@babel/types";
-import React, { useEffect, useState } from "react";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import { getDeviceByImei } from "../services/deviceService";
-function Scanner() {
-  const [data, setData] = useState("Not Found");
-  const [torchOn, setTorchOn] = useState(false);
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (data === "Not Found") return;
-    if (data.length === 15) {
-      getDeviceData(data);
-    }
-  }, [data]);
-  const getDeviceData = async (data) => {
-    try {
-      const result = await getDeviceByImei(data);
-      if (result?._id) {
-        navigate(`/devices/viewDevice/${result?._id}`);
-      }
-    } catch {
-      toast.error("Device Not Found");
-    }
-  };
+import { useState } from "react";
+import { useZxing } from "react-zxing";
+
+const Scanner = () => {
+  const [result, setResult] = useState("");
+  const { ref } = useZxing({
+    onResult(result) {
+      setResult(result.getText());
+    },
+  });
+
   return (
     <>
-      <BarcodeScannerComponent
-        height={500}
-        torch={torchOn}
-        onUpdate={(err, result) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
-        }}
-      />
-      <p>{data}</p>
-      <button onClick={() => setTorchOn(!torchOn)}>
-        Switch Torch {torchOn ? "Off" : "On"}
-      </button>
+      <video ref={ref} />
+      <p>
+        <span>Last result:</span>
+        <span>{result}</span>
+      </p>
     </>
   );
-}
+};
 
 export default Scanner;
