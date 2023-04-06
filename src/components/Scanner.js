e436ff0b4f0b0bc8c2c1984fd9463c88ac68def2
nitheish-ss <nitheishm@gmail.com>
2023-04-06 17:10:16 +0530

    torch

--- src/components/Scanner.js
+++ src/components/Scanner.js
@@ -1,23 +1,53 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { getDeviceByImei } from "../services/deviceService";
 import { useZxing } from "react-zxing";
-
-const Scanner = () => {
-  const [result, setResult] = useState("");
-  const { ref } = useZxing({
+function Scanner() {
+  const [data, setData] = useState("Not Found");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (data === "Not Found") return;
+    if (data.length === 15) {
+      getDeviceData(data);
+    }
+  }, [data]);
+  const {
+    ref,
+    torch: { on, off, isOn, isAvailable },
+  } = useZxing({
     onResult(result) {
-      setResult(result.getText());
+      setData(result.getText());
     },
   });
-
+  const getDeviceData = async (data) => {
+    try {
+      const result = await getDeviceByImei(data);
+      if (result?._id) {
+        navigate(`/devices/viewDevice/${result?._id}`);
+      }
+    } catch {
+      toast.error("Device Not Found");
+    }
+  };
   return (
     <>
-      <video ref={ref} />
-      <p>
-        <span>Last result:</span>
-        <span>{result}</span>
-      </p>
+      <div className="col">
+        <video height={500} ref={ref} />
+        <p>{data}</p>
+        {isAvailable ? (
+          <button
+            className="btn btn-secondary"
+            onClick={() => (isOn ? off() : on())}
+          >
+            {isOn ? "Turn off" : "Turn on"} torch
+          </button>
+        ) : (
+          <strong>Please Turn on Torch If lighting is Bad.</strong>
+        )}
+      </div>
     </>
   );
-};
+}
 
 export default Scanner;
